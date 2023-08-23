de11598f7ebd41a65d23fb4e6bdb1e0e9be04ac3
Panji <panji.bp.bp@gmail.com>
2023-08-23 23:03:27 +0700

    Fixing.

--- handler/campaign.go
+++ handler/campaign.go
@@ -31,7 +31,6 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 
 	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
-	return
 }
 
 func (h *campaignHandler) GetCampaign(c *gin.Context) {
